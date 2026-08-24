47840817dd52322f934fef73c515777df39eb25c
Chinomso-Onwubiko <chinomso.onwubiko@pau.edu.ng>
2026-08-24 22:27:08 +0100

    This is another commit, probably my 4th one)

---
+++ learning_cargo/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    println!("Welcome, may I know your first name?");
+
+    let my_name = "Chinomso";
+    let mut fname = String::new();
+    io::stdin()
+        .read_line(&mut fname)
+        .expect("The user gave no input");
+
+    println!("What's your surname?");
+    let mut sname = String::new();
+    io::stdin()
+        .read_line(&mut sname)
+        .expect("The second user's input could not be stored.");
+
+    println!("Hello {} {}.I'm {}, it's nice to meet you.", fname.trim(), sname.trim(), my_name.trim());
+}
